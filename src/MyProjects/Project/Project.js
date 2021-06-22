fb35f41f29ce7a6ceff998625d70c1df028de0c9
Jolud95 <www.appel@mail.ru>
2021-06-22 19:00:52 +0300

    Test account data

--- src/MyProjects/Project/Project.js
+++ src/MyProjects/Project/Project.js
@@ -10,6 +10,9 @@ function Project(props) {
             <div className={style.info}>
                 <h3 className={style.title}>{props.title}</h3>
                 <p className={style.description}>{props.description}</p>
+                <p>Test account data:<br/>
+                    Email: free@samuraijs.com<br/>
+                    Password: free</p>
             </div>
         </div>
     );
